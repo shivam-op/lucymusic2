b55271cc8ee796c82b6e75e8f470f5ca4a148235
Rishabh anand <90849358+rishabhanand2@users.noreply.github.com>
2021-10-22 20:39:50 +0530

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -5,5 +5,5 @@ from pyrogram.types import Chat, Message, User
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Hello This Is My Music Assistance Id Dont Spam 😑")
+  await USER.send_message(message.chat.id,"Hello my name is Lucy x player I am play music on group first add lucy bot to your group and make admin and play songs")
   return                        
